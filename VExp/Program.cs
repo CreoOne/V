65095e56db0f529cc2ec6a0d9d7e508a892bb2ca
Tomasz Czarnecki <creo.one@gmail.com>
2017-09-24 17:14:59 +0200

    VExp code reorganisation.

--- VExp/Program.cs
+++ VExp/Program.cs
@@ -11,10 +11,21 @@ namespace VExp
     {
         static void Main(string[] args)
         {
+            ExpVectorCross();
+            ExpVectorDimensionalityChanges();
+            ExpVectorArithmetic();
+            ExpVectorGeometry();
+        }
+
+        public static void ExpVectorCross()
+        {
             Vector cross2d = Vector.Cross(new Vector(1, 0));
             Vector cross3d = Vector.Cross(new Vector(1, 0, 0), new Vector(0, 1, 0));
             Vector cross4d = Vector.Cross(new Vector(1, 0, 0, 0), new Vector(0, 1, 0, 0), new Vector(0, 0, 1, 0));
+        }
 
+        private static void ExpVectorDimensionalityChanges()
+        {
             Vector addDimension = Vector.Insert(new Vector(2, 2), 0, 1);
             Vector removeDimension = Vector.Remove(new Vector(2, 0, 2), 1);
 
@@ -24,6 +35,12 @@ namespace VExp
             Vector postfixDimension = Vector.Postfix(new Vector(2, 2), 1);
             Vector unpostfixDimension = Vector.UnPostfix(new Vector(2, 2, 1));
 
+            Vector[] swap1 = Vector.Swap(new Vector(1, 2, 3));
+            Vector[] swap2 = Vector.Swap(new Vector(1, 2, 3), new Vector(4, 5, 6));
+        }
+
+        private static void ExpVectorArithmetic()
+        {
             Vector add1 = new Vector(1, 2) + new Vector(2, 1);
             Vector add2 = new Vector(1, 2) + 2;
             Vector add3 = 2 + new Vector(2, 1);
@@ -42,14 +59,14 @@ namespace VExp
 
             Vector invert = -new Vector(1, 2, 3);
 
-            Vector normalize = Vector.Normalize(new Vector(1, 1));
-            double dot = Vector.Dot(new Vector(1, 0), new Vector(0.5, 0.5));
-
-            Vector[] swap1 = Vector.Swap(new Vector(1, 2, 3));
-            Vector[] swap2 = Vector.Swap(new Vector(1, 2, 3), new Vector(4, 5, 6));
-
             Vector min = Vector.Min(new Vector(-1, 1, -1), new Vector(1, -1, 1));
             Vector max = Vector.Max(new Vector(-1, 1, -1), new Vector(1, -1, 1));
+        }
+
+        private static void ExpVectorGeometry()
+        {
+            Vector normalize = Vector.Normalize(new Vector(1, 1));
+            double dot = Vector.Dot(new Vector(1, 0), new Vector(0.5, 0.5));
         }
     }
 }
